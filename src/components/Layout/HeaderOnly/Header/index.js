f0a5c74b32a8342ddf837a8cfdcd0060252eb12b
unknown <huynguyen20041119007@gmail.com>
2024-03-25 23:39:21 +0700

    upload

---
+++ src/components/Layout/HeaderOnly/Header/index.js
@@ -0,0 +1,5 @@
+function Header() {
+    return <h2>Header only</h2>;
+}
+
+export default Header;
---
+++ src/routes/index.js
@@ -0,0 +1,25 @@
+//layouts
+import { HeaderOnly } from "~/components/Layout";
+
+//pages
+import Home from "~/page/Home";
+import Following from "~/page/Following";
+import Profile from "~/page/Profile";
+import Upload from "~/page/Upload";
+import Search from "~/page/Search";
+
+//không cần đăng nhập vẫn vào được
+const publicRoutes = [
+    { path: '/', component: Home},
+    { path: '/Following', component: Following},
+    { path: '/profile', component: Profile},
+    { path: '/upload', component: Upload, layout: HeaderOnly},
+    { path: '/search', component: Search, layout: null},
+];
+
+//cần đăng nhập để vào
+const privateRouters = [
+
+];
+
+export { publicRoutes, privateRouters };
